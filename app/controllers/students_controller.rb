020555bc347edaf4d634651dffb29d70be86ba85
Justin Chittarath <justinchitt@gmail.com>
2021-11-22 10:24:28 -0700

    commit tree 89b43928210e3365954636ea02185cc9a87951ec

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,16 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+  def query
+    if student.find_by_first_name(params[:name])
+      render json: student.find_by_first_name(params[:name])
+    else
+      render json: student.find_by_last_name(params[:name])
+    end
+  end
 
 end
